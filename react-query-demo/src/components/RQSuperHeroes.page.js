1721f55637e8b3e86b23bc4f9c3cb99e1c426ad3
Akanksha Patel <a.patel@easternenterprise.com>
2022-01-21 16:53:20 +0530

    success and error callbacks

--- react-query-demo/src/components/RQSuperHeroes.page.js
+++ react-query-demo/src/components/RQSuperHeroes.page.js
@@ -5,14 +5,22 @@ const fetchSuperHeroes = () => {
   return axios.get("http://localhost:4000/superheroes");
 };
 
+const onSuccess = (data) => {
+  console.log('Perform side effect after data fetching', data);
+}
+
+const onError = (error) => {
+  console.log('Perform side effect after encountering error', error);
+}
+
 export const RQSuperHeroesPage = () => {
-  const { isLoading, data, error, isError, isFetching } = useQuery("super-heroes", fetchSuperHeroes, 
-  { refetchInterval: 5000,
-    refetchIntervalInBackground: true });
+  const { isLoading, data, error, isError, isFetching, refetch } = useQuery("super-heroes", fetchSuperHeroes, 
+  { onSuccess: onSuccess,
+    onError: onError });
 
   console.log({isLoading, isFetching})
 
-  if (isLoading) {
+  if (isLoading || isFetching) {
     return <h2>Loading...</h2>;
   }
 
@@ -23,6 +31,7 @@ export const RQSuperHeroesPage = () => {
   return (
     <>
       <h2>RQ Super Heroes Page</h2>
+      <button onClick={refetch}>Fetch Heros</button>
       {data.data.map((hero) => {
         return <div>{hero.name}</div>;
       })}
